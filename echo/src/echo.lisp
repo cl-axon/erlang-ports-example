49e0f6155341a7621545ac25a1b3815b414cb681
Duncan McGreggor <oubiwann@gmail.com>
2020-07-06 23:27:01 -0500

    Use the constants, Luke.

--- echo/src/echo.lisp
+++ echo/src/echo.lisp
@@ -8,10 +8,10 @@
 (defun setup-app ()
   (let ((log-level echo-const:*log-level*))
     ;; (echo-util:reset-stdout)
-    (log:config log-level :daily echo-const:*debug-file*  :backup nil)))
+    (log:config log-level :daily echo-const:*debug-file* :backup nil)))
 
 (defun run ()
   (log:info "Starting Lisp language server ...")
-  (let ((io *terminal-io*))
+  (let ((io echo-const:*input*))
     (loop while
       (echo-protocol::process-port-messages echo-const:*delimiter* io))))
